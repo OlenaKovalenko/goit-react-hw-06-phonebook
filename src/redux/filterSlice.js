0df9324e6d57619124d7413c722a52eab242e085
OlenaKovalenko <elenkow17@gmail.com>
2023-12-02 22:29:33 +0200

    Add filterSlice.js

--- src/redux/filterSlice.js
+++ src/redux/filterSlice.js
@@ -10,4 +10,7 @@ export const filterSlice = createSlice({
             }
         }
     }
-})
+});
+
+export const { changeFilter } = filterSlice.actions;
+export const filterReducer = filterSlice.reducer;
