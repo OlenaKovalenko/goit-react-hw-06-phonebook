2ac798496354bc747fb785657e8034c8bc488f87
OlenaKovalenko <elenkow17@gmail.com>
2023-12-04 22:43:52 +0200

    Update Filter.js

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -7,11 +7,16 @@ export const Filter = () => {
     const filter = useSelector(getFilter);
     const dispatch = useDispatch();
 
-    const handleFilterChange = filter => dispatch(changeFilter(filter));
     return (
         <>
             <FilterTitle>Find contacts by name</FilterTitle>
-            <FilterInput type="text" name="filter" id="filter" value={filter} required onChange={(e) => handleFilterChange(e.target.value)}/>
+            <FilterInput
+                type="text"
+                name="filter"
+                id="filter"
+                value={filter}
+                required
+                onChange={e => dispatch(changeFilter(e.target.value))} />
         </>
     )
 }
