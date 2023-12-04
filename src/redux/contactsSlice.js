1be9e93b20bad433ffc0620d97fc8c073f7b85c9
OlenaKovalenko <elenkow17@gmail.com>
2023-12-04 21:44:42 +0200

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -34,6 +34,8 @@ export const contactsSlice = createSlice({
     }
 });
 
+
+
 export const { addContact, deleteContact } = contactsSlice.actions;
 export const contactsReducer = contactsSlice.reducer;
 
